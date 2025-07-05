427a23ffd4787b1c7f578705789be202d605ed82
gaurav thapa <gaurav16547@gmail.com>
2025-07-05 18:51:21 +0545

    expanded signup to handle signup as well as jwttoken'

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,45 @@
-export const signup = (req, res) => {};
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters long" });
+    }
+    const user = await User.findOne(email);
+    if (user) {
+      return res.status(400).json({ message: "Email already exist." });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data." });
+    }
+  } catch (error) {
+    console.log("Error in signup " + error);
+    res.status(500).json({ message: "Internal Server Errore" });
+  }
+};
 
 export const login = (req, res) => {
   res.send("login route");
