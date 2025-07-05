1c2389c88aad69447c2fee2de5bade690eeb337f
gaurav thapa <gaurav16547@gmail.com>
2025-07-05 18:17:05 +0545

    used mongodb for first time

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "../routes/auth.route.js";
+import { connectDB } from "../lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -9,4 +10,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  connectDB();
 });
