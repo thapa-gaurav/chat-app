176c202c0bb808b085dc56fd2652cee89298a890
gaurav thapa <gaurav16547@gmail.com>
2025-07-05 17:57:32 +0545

    added dynamic port option

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "../routes/auth.route.js";
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT;
+app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Server is running on port 50001");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
