45a276f9a4bf01b5aa6e9b34dab285206e7891e6
gaurav thapa <gaurav16547@gmail.com>
2025-07-06 11:21:14 +0545

    added message endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "../routes/auth.route.js";
+import messageRoutes from "../routes/message.route.js";
 import { connectDB } from "../lib/db.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT;
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
