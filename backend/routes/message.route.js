81e05060cf84270be9e878bbcf400ee4a26e8bf5
gaurav thapa <gaurav16547@gmail.com>
2025-07-08 20:20:52 +0545

    added message route such as getUsers, getMessages and sendMessage

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,15 @@
 import express, { Router } from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getUsersForSideBar,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const messageRoutes = express.Router();
+
+messageRoutes.get("/users", protectRoute, getUsersForSideBar);
+messageRoutes.get("/:id", protectRoute, getMessages);
+messageRoutes.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRoutes;
