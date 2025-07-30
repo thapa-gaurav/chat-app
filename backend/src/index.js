ee343f8fa597599b4e521b6c8225a5171765e4be
gaurav thapa <gaurav16547@gmail.com>
2025-07-30 18:26:53 +0545

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/auth", authRoutes);
