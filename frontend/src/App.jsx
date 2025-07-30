8b7065b87613e1325eb1329b3ee1ea58a7792a8d
gaurav thapa <gaurav16547@gmail.com>
2025-07-30 13:10:19 +0545

    added more

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader } from "lucide-react";
-
+import { Toaster } from "react-hot-toast";
 export const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
@@ -49,6 +49,7 @@ export const App = () => {
           element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
+      <Toaster />
     </div>
   );
 };
