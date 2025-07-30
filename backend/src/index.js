4d3b1089c5e322824e5bd3d2d188fc5fe78be360
gaurav thapa <gaurav16547@gmail.com>
2025-07-30 11:29:57 +0545

    added all files

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import authRoutes from "../routes/auth.route.js";
 import messageRoutes from "../routes/message.route.js";
 import { connectDB } from "../lib/db.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -12,6 +12,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5174", Credential: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -6,8 +6,24 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
 
 export const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log(authUser);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
   return (
     <div>
       <Navbar />
