9d46f6000f1d577114877e453aeb8682baf678c0
gaurav thapa <gaurav16547@gmail.com>
2025-08-02 08:55:48 +0545

    added chatheader and message ui no functionality

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/usechatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+  if (isMessagesLoading)
+    return (
+      <div>
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto ">
+      <ChatHeader />
+      <p>messages...</p>
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
--- frontend/src/store/usechatStore.js
+++ frontend/src/store/usechatStore.js
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -29,7 +29,19 @@ export const useChatStore = create((set) => ({
     } catch (error) {
       toast.error(error.response?.data?.message || error.message);
     } finally {
-      set({ isMessageLoading: false });
+      set({ isMessagesLoading: false });
+    }
+  },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response?.data?.message || error.message);
     }
   },
   setSelectedUser: (selectedUser) => set({ selectedUser }),
